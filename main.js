255e3727d97a0ba09f6e42892310169c590d2137
hyoga-dev <75929898+hyoga-dev@users.noreply.github.com>
2022-09-27 18:34:53 +0700

    Up js 2

--- main.js
+++ main.js
@@ -8,9 +8,9 @@ const url = "https://bespoke-douhua-40ca24.netlify.app"
   // * TAMPILKAN DATA SAAT LOAD --
   // * -----------------------------------------------------
   fetch('https://bespoke-douhua-40ca24.netlify.app') 
-      .then(res => {res.json(); console.log(res.json)})
+      .then(res => res.json())
       .then(data => {
-          // console.log(data)
+          console.log(data)
         // let isi = ''
         // for (let i = 0; i < data.length; i++){
 
